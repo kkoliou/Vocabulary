3de60add5cb9b8b38d9674b954120f0f2f9ec7da
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2026-01-20 23:02:17 +0200

    Initial Commit

---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Konstantinos Kolioulis on 20/1/26.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocabularyTests/VocabularyTests.swift
@@ -0,0 +1,16 @@
+//
+//  VocabularyTests.swift
+//  VocabularyTests
+//
+//  Created by Konstantinos Kolioulis on 20/1/26.
+//
+
+import Testing
+
+struct VocabularyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
