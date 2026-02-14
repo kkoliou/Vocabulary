b4984e0d9fef50e57ce5d946468fe5dcc8596724
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2026-02-14 20:50:26 +0200

    build: change project structure by creating one main module which contains submodules

---
+++ Vocabulary/VocabularyModules/Package.swift
@@ -0,0 +1,81 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import Foundation
+import PackageDescription
+
+var package = Package(
+  name: "VocabularyModules",
+  platforms: [
+    .iOS(.v18)
+  ],
+  products: [
+    // Shared/Core products
+    .library(name: "Shared", targets: ["Shared"]),
+    .library(name: "VocabularyDB", targets: ["VocabularyDB"]),
+    .library(name: "VocabularyCsvParser", targets: ["VocabularyCsvParser"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/sqlite-data", from: "1.4.2")
+  ],
+  targets: [
+    // MARK: - Shared/Core targets
+    .target(
+      name: "Shared"
+    ),
+    .testTarget(
+      name: "SharedTests",
+      dependencies: ["Shared"]
+    ),
+    .target(
+      name: "VocabularyDB",
+      dependencies: [
+        .product(name: "SQLiteData", package: "sqlite-data")
+      ]
+    ),
+    .testTarget(
+      name: "VocabularyDBTests",
+      dependencies: ["VocabularyDB"]
+    ),
+    .target(
+      name: "VocabularyCsvParser"
+    ),
+    .testTarget(
+      name: "VocabularyCsvParserTests",
+      dependencies: ["VocabularyCsvParser"]
+    ),
+  ]
+)
+
+// MARK: - Feature modules
+package.products.append(contentsOf: [
+  .library(name: "VocabularyFeature", targets: ["VocabularyFeature"]),
+  .library(name: "VocabulariesFeature", targets: ["VocabulariesFeature"]),
+])
+
+package.targets.append(contentsOf: [
+  .target(
+    name: "VocabularyFeature",
+    dependencies: [
+      "VocabularyDB",
+      "Shared",
+      "VocabularyCsvParser"
+    ]
+  ),
+  .testTarget(
+    name: "VocabularyFeatureTests",
+    dependencies: ["VocabularyFeature"]
+  ),
+  .target(
+    name: "VocabulariesFeature",
+    dependencies: [
+      "VocabularyDB",
+      "Shared",
+      "VocabularyFeature"
+    ]
+  ),
+  .testTarget(
+    name: "VocabulariesFeatureTests",
+    dependencies: ["VocabulariesFeature"]
+  ),
+])
