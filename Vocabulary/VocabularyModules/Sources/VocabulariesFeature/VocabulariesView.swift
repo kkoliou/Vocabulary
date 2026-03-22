8a7e572c73aa62e3a73bd2780ea220e561c80b95
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2026-03-22 15:34:32 +0200

    chore: string change

--- Vocabulary/VocabularyModules/Sources/VocabulariesFeature/VocabulariesView.swift
+++ Vocabulary/VocabularyModules/Sources/VocabulariesFeature/VocabulariesView.swift
@@ -65,7 +65,7 @@ public struct VocabulariesView: View {
             .font(AppTypography.title3)
         },
         description: {
-          Text(Strings.localized("Add English-Greek vocabularies to get started"))
+          Text(Strings.localized("Load English-Greek vocabularies to get started"))
             .font(AppTypography.subheadline)
         },
         actions: {
@@ -80,7 +80,7 @@ public struct VocabulariesView: View {
                 Image(systemName: "plus.circle.fill")
                   .font(AppTypography.callout)
                 
-                Text(Strings.localized("Add vocabularies"))
+                Text(Strings.localized("Load vocabularies"))
                   .font(AppTypography.callout.weight(.semibold))
               }
               .opacity(viewModel.isAddSampleVocabsLoading ? 0 : 1)
